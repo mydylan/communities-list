c970080a18b0161064df7e21858c14df11c573c1
Taras Kostiuchenko <mydylan7@gmail.com>
2019-09-11 16:55:37 +0300

    Init react application

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <React.Fragment>
+      <header>
+        Communities
+      </header>
+    </React.Fragment>
+  );
+}
+
+export default App;
